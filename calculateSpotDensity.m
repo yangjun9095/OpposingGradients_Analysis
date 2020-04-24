e033748813c8e6073e8e3fbb592f9828f45af21d
yangjun9095 <yjkim90@berkeley.edu>
2020-04-24 13:16:28 -0700

    Update

--- calculateSpotDensity.m
+++ calculateSpotDensity.m
@@ -3,12 +3,17 @@ function calculateSpotDensity(DataType,varargin)
 % This function is to calculate the spot density 
 % spot density = # of spots / area of APbins
 % Caveats : 
-% 1) we need to ignore APbins whose area is just too small, compared
+% 1) We need to ignore APbins whose area is just too small, compared
 % to the half of the maximum APbin area.
 % 2) 
+% Assumptions (that could be relaxed later)
+% 1) We will only think about NC14 as our current Runt null datasets have
+% only have NC14 intact. Thus, we will synchronize all the vectors to the
+% beginning of NC14 (In other words, the CheckDivisionTimes.m is very important).
+% 2) For a long enough time series, we will assume 
 
 %% Method
-% Get the number of spots per AP bin at each frame (nSpots)
+% Get the number of spots/particles per AP bin at each frame (nParticles)
 % Get the APbin area for corresponding AP bins (areaAPbins)
 % Then, get the ratio of "nSpots/areaAPbins"
 % We call it as density. For specific datasets, we take the maximum density
@@ -21,8 +26,13 @@ function calculateSpotDensity(DataType,varargin)
 
 %% Method1.
 
+% Temporary
+DataType = 'r3_RuntNull';
+
 % Load the datasets
 Data = LoadMS2Sets(DataType)
 
+% Extract useful fields
+NParticles = Data.NParticlesAP;
 
 end
---
+++ main_normalize_expression_mostAnteriorPoints.m
@@ -0,0 +1,15 @@
+function main_normalize_expression_mostAnteriorPoints
+%% Description 
+% This script is for exploring the fold-change idea of 17.5% of APbin as a
+% proxy for Runt nulls. 
+
+%% 
+% 1) Take 17.5% of APbin's mean spot fluo data for each dataset.
+% 2) Divide the mean spot fluo from each AP bin with 1)
+% 3) First, compile all datasets with their sex
+% Start with [000], [111], then move to [100]/[010]/[001]
+% Caveat : Pick female embryos only at this moment.
+
+%% Load the datasets
+
+end
