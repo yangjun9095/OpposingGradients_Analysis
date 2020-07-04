27f937abbc3e89e570a97cc3d475c28f01ba0c32
yangjun9095 <yjkim90@berkeley.edu>
2020-07-04 14:40:47 -0700

    Update ImageAnalysis_pipeline_forSharing.m

--- ImageAnalysis_pipeline_forSharing.m
+++ ImageAnalysis_pipeline_forSharing.m
@@ -6,4 +6,19 @@
 % Note that now we don't have to export the .lif files to .tif files with
 % LASX.
 Prefix = ExportDataForLivemRNA
-% Click the folder containing the 
+% Click the folder containing the raw movie (.lif file, .lif file does not
+% have to be exported.)
+
+%% filtering movie
+filterMovie(Prefix,'highPrecision','customFilter','Difference_of_Gaussian_3D','nWorkers',1)
+
+%% spot segmentation
+% Thresh = ? % after ImageJ
+segmentSpots(Prefix, Thresh,'Shadows',1,'keepProcessedData')%'fit3D','displayFigures')
+
+%% TrackNuclei
+
+%% TrackmRNADynamics
+
+%% CompileParticle
+CompileParticles(Prefix,'skipAll','ApproveAll','MinParticles',2,'MinTime',2)
