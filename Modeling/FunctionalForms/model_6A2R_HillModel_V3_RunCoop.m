f2e1f93606671577453599024dccbc994c96d94c
yangjun9095 <yjkim90@berkeley.edu>
2021-01-02 13:33:52 -0800

    update on 1/2/2021

--- Modeling/FunctionalForms/model_6A2R_HillModel_V3_RunCoop.m
+++ Modeling/FunctionalForms/model_6A2R_HillModel_V3_RunCoop.m
@@ -1,4 +1,4 @@
-function [Rate] = model_6A2R_HillModel_V3(params,TF) 
+function [Rate] = model_6A2R_HillModel_V3_RunCoop(params,TF) 
 
 %% Definition of parameters
 % [Kb, w_bp, p, R_max, Kr1, Kr2, w_rp1, w_rp2] = params;
@@ -12,6 +12,8 @@ Kr1 = params(5);
 Kr2 = params(6);
 w_rp1 = params(7);
 w_rp2 = params(8);
+% Run-Run cooperativity
+w_rr = params(9);
 
 
 % TF inputs : Read a matrix of TF, each column represent different TFs
@@ -25,15 +27,15 @@ r2 = Runt./Kr2;
 
 % Calculate the P_bound, and Rate
 Z = 1 + p + b.^6 + r1 + r2 +...
-    b.^6*(r1 + r2 + p*w_bp) + r1*p*w_rp1 + r2*p*w_rp2 + r1*r2+...
-    r1*r2*p*w_rp1*w_rp2 + b.^6*p*w_bp*(r1*w_rp1 + r2*w_rp2) + b.^6*r1*r2+...
-    b.^6*r1*r2*p*w_bp*w_rp1*w_rp2;
+    b.^6.*(r1 + r2 + p*w_bp) + r1*p*w_rp1 + r2*p*w_rp2 + r1.*r2*w_rr+...
+    r1.*r2*p*w_rp1*w_rp2*w_rr + b.^6*p*w_bp.*(r1*w_rp1 + r2*w_rp2) + b.^6.*r1.*r2*w_rr+...
+    b.^6.*r1.*r2*p*w_bp*w_rp1*w_rp2*w_rr;
 
-Z_p = p +  b.^6*p*w_bp + r1*p*w_rp1 + r2*p*w_rp2 
+Z_p = p +  b.^6*p*w_bp + r1*p*w_rp1 + r2*p*w_rp2 +...
+        r1.*r2*p*w_rp1*w_rp2*w_rr + b.^6*p*w_bp.*(r1*w_rp1 + r2*w_rp2)+...
+         b.^6.*r1.*r2*p*w_bp*w_rp1*w_rp2*w_rr;
 
-P_bound = 
-P_bound = p*(1+b.^6*w_bp + r*w_rp + b.^6.*r*w_bp*w_rp)./...
-            (1 + p + b.^6 + r + b.^6.*r + b.^6*p*w_bp + r*p*w_rp + b.^6.*r*p*w_bp*w_rp);
+P_bound = Z_p./Z;
         
 Rate = R_max* P_bound;
 
--- Modeling/predict_6A2R_HillV3.m
+++ Modeling/predict_6A2R_HillV3.m
@@ -25,7 +25,7 @@ Bicoid = TFinput(:,1);
 Runt = TFinput(:,2);
 RuntNull = TFinput(:,3);
 
-%% Fit the Runt null to extract the Bcd dependent parameters
+%% Fit the "Runt null" only to extract the Bcd dependent parameters
 % Kb, w_bp, p, R_max
 
 % Default settings for the MCMC
@@ -70,7 +70,7 @@ for construct = [3, 7, 8] % indices from the data.mat
     % Range that is set as an initial guess. We will get a common set of
     % APbins that does not have NaN values in these AP bins.
     APbin_start = 20/2.5 + 1;
-    APbin_end = 45/2.5 + 1;
+    APbin_end = 50/2.5 + 1;
 
     APbinRange = (APbin_start:APbin_end)';
 
@@ -94,18 +94,18 @@ for construct = [3, 7, 8] % indices from the data.mat
     % Decide whether we want to fit the Runt null/WT data together or not.
     % depending on this, we will set the xdata and ydata for the fitting.
     MCMCdata = struct;
-    MCMCdata.APdata = [APbins' ; APbins'];
-    MCMCdata.ydata = [Rate_null ; Rate_WT];
+    MCMCdata.APdata = [APbins'];
+    MCMCdata.ydata = [Rate_null];
     % input TF
-    MCMCdata.Bcd = [Bcd ; Bcd];
-    MCMCdata.Run = [RunNull ; Run];
+    MCMCdata.Bcd = [Bcd];
+    MCMCdata.Run = [RunNull];
     MCMCdata.xdata = [MCMCdata.Bcd, MCMCdata.Run];
 
     MCMCdata.R_max = max(MCMCdata.ydata);
     MCMCdata.R_min = min(MCMCdata.ydata);
 
     % Pick a model for the fitting
-    mdl = @(TF, params) model_6A1R_HillModel_V3(params, TF);
+    mdl = @(TF, params) model_6A2R_HillModel_V3(params, TF);
 
     %leaving this here in case it'll be useful in the future
     model.ssfun = @(params, data) sum((data.ydata-mdl(data.xdata, params)).^2);
@@ -115,7 +115,7 @@ for construct = [3, 7, 8] % indices from the data.mat
     % Define the parameters for the model
     % put the initial parameters and bounds in a form that the mcmc function
     % accepts
-    names = {'K_{b}', 'K_{r}', 'w_bp', 'w_rp', 'p', 'R_{max}'};
+    names = {'K_{b}', 'w_{bp}', 'p', 'R_max', 'K_{r1}', 'K_{r2}', 'w_{rp1}', 'w_{rp2}'};
     params = cell(1, length(names));
 
     % Initialize MCMC parameters.
@@ -127,11 +127,11 @@ for construct = [3, 7, 8] % indices from the data.mat
     R_max0 = MCMCdata.R_max; 
     % R_min0 = MCMCdata.R_min;
 
-    params0 = [Kb0, Kr0, w_bp0, w_rp0, p0, R_max0];
+    params0 = [Kb0, w_bp0, p0, R_max0, Kr0, Kr0, w_rp0, w_rp0];
 
     % Bounds of the parameters
-    LB = [0.1, 0.1, 1, 0, 0, 50];
-    UB = [10^2, 10^2, 10^2, 1.2, 1, 500];
+    LB = [0.1, 1, 0, 50, 0.1, 0.1, 0, 0];
+    UB = [10^2, 10^2, 0.5, 500, 10^2, 10^2, 1, 1];
 
 
     for i = 1:length(names)
@@ -141,11 +141,11 @@ for construct = [3, 7, 8] % indices from the data.mat
         targetflag = 1; %is this optimized or not? if this is set to 0, the parameter stays at a constant value equal to the initial value.    
         localflag = 0; %is this local to this dataset or shared amongst batches?
 
-        if i==6
+        if i==4
             pri_mu = MCMCdata.R_max;
             pri_sig = 20;
     %         targetflag = 0; % Fix this parameter
-        elseif i==2 || i==4
+        elseif i>4
             pri_mu = NaN;
             pri_sig = Inf;
             targetflag = 0; % Fix this parameter
@@ -173,7 +173,8 @@ for construct = [3, 7, 8] % indices from the data.mat
 
 
     % Initialize the covariance matrix
-    J0 = diag([Kb_step, Kr_step, w_bp_step, w_rp_step, p_step, R_max_step]);
+    J0 = diag([Kb_step, w_bp_step, p_step, R_max_step,...
+                Kr_step, Kr_step, w_rp_step, w_rp_step]);
 
     % Run the MCMC (this whole block can be inserted inside the for loop above,
     % to run the MCMC for different constructs.
@@ -199,6 +200,28 @@ end
 datanum = 1;
 chain = MCMC_2RunSites(datanum).chain;
 results = MCMC_2RunSites(datanum).results;
+
+%% Check the raw fit quickly (for the Runt nulls)
+datanum = 1;
+chain = MCMC_2RunSites(datanum).chain;
+params_bcd = mean(chain_temp(n_burn+1:end,:)); % [Kb, w_bp, p, R_max];
+% params_temp = [params_bcd, 10, 10, 1, 1];
+% params_temp = [86.9719   48.7446    0.6287  174.8984   10.0000   10.0000    1.0000    1.0000]
+params_temp = [86.9719   48.7446    0.3287  174.8984   10.0000   10.0000    1.0000    1.0000]
+
+Rate_null_fit = model_6A2R_HillModel_V3(params_temp, MCMCdata.xdata);
+
+% data
+constructs_list = [3,7,8];
+construct = constructs_list(datanum); 
+Rate_null = compiledData{construct+1+8,9};
+Rate_null_SEM = compiledData{construct+1+8,10};
+    
+hold on
+% Runt nulls
+errorbar(APaxis, Rate_null, Rate_null_SEM,'o','Color',ColorChoice(4,:),'CapSize',0,'MarkerFaceColor',ColorChoice(4,:))
+% Runt null fit
+plot(APaxis(APbins_fit), Rate_null_fit)
 %% Diagnose the MCMC result
 % stats = chainstats(chain,results);
 n_burn = 0.5*n_steps;
@@ -305,9 +328,115 @@ for i=1:3
     StandardFigure(gcf,gca)
     pause
 %   %Save the plot
+    saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.tif']); 
+    saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.pdf']);
+    pause
+end
+
+%% Predict the Rates with Run-Run cooperativity
+% mnodel_6A2R_HillModelV3_RunCoop.m
+% w_rr > 1 : stronger repression than independent Runt binding.
+w_rr = [1, 10^6, 10^12, 10^24];
+
+% Predict the level for the Runt WT
+% Use a custom-function for the model : model_6A2R_HillV3
+% [Kb, w_bp, p, R_max, Kr1, Kr2, w_rp1, w_rp2] = params
+
+n_simu = length(MCMC_2RunSites(1).chain);
+n_burn = 0.5*n_simu;
+
+% r2-new : [011]
+% r2-close : [110]
+% r2-far : [101]
+% Predict the cases of 2 binding sites
+Prediction = zeros(3, length(w_rr), 82);
+for i=1:3
+    chain_temp = MCMC_2RunSites(i).chain;
+    params_Bcd = mean(chain_temp(n_burn+1:end,:)); % [Kb, w_bp, p, R_max];
+    
+    if i==1 % r2-new : [011]
+        params_temp1 = params_MCMC(2,:); %[001]
+        params_temp2 = params_MCMC(3,:); %[010]
+    elseif i==2 % r2-close : [110]
+        params_temp1 = params_MCMC(1,:); %[100]
+        params_temp2 = params_MCMC(3,:); %[010]
+    elseif i==3 % r2-far : [101]
+        params_temp1 = params_MCMC(1,:); %[100]
+        params_temp2 = params_MCMC(2,:); %[001]
+    end
+    Kr1 = params_temp1(2);
+    w_rp1 = params_temp1(4);
+    Kr2 = params_temp2(2);
+    w_rp2 = params_temp2(4);
+    
+    for j=1:length(w_rr)
+        omega_rr = w_rr(j);
+
+        params_6A2R = [params_Bcd, Kr1, Kr2, w_rp1, w_rp2, omega_rr];
+
+        Prediction(i,j,:) = model_6A2R_HillModel_V3_RunCoop(params_6A2R, TF_combined);
+    end
+end
+
+%% 
+
+
+for i=1:3
+    % construct define
+    if i==1
+        construct = 3;
+    elseif i==2
+        construct = 7;
+    elseif i==3
+        construct = 8;
+    end
+    
+    clf
+    
+   % initialize the variables
+    Rate = [];
+    Rate_SEM = [];
+    Rate_null = [];
+    
+    % Pull the relevant data from the compiledData
+    Rate = compiledData{construct+1,9};
+    Rate_SEM = compiledData{construct+1,10};
+
+    Rate_null = compiledData{construct+1+8,9};
+    Rate_null_SEM = compiledData{construct+1+8,10};
+    
+    hold on
+    % Runt nulls
+    errorbar(APaxis, Rate_null, Rate_null_SEM,'o','Color',ColorChoice(4,:),'CapSize',0,'MarkerFaceColor',ColorChoice(4,:))
+    % Runt WT
+    errorbar(APaxis, Rate, Rate_SEM,'o','Color',ColorChoice(1,:),'CapSize',0,'MarkerFaceColor',ColorChoice(1,:))
+    
+    % Run-Run interaction strength
+    for j=1:length(w_rr)
+
+        % Prediction
+        % Runt null
+        plot(APaxis, squeeze(Prediction(i,j,1:41)),'Color',ColorChoice(4,:), 'LineWidth', 2)
+        % Runt WT
+        plot(APaxis, squeeze(Prediction(i,j,42:end)),'Color',ColorChoice(1,:), 'LineWidth', 2)
+        pause
+    end
+
+    xlim([0.2 0.6])
+    xticks([0.2 0.3 0.4 0.5 0.6])
+    ylim([0 400])
+    yticks([0 100 200 300 400])
+
+    xlabel('embryo length')
+    ylabel({'initial RNAP', 'loading rate (AU/min)'})
+    
+    box on
+    StandardFigure(gcf,gca)
+    pause
+%   %Save the plot
 %     saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.tif']); 
 %     saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.pdf']);
-    pause
+%     pause
 end
 
 %% Approach 2 : fixing the Kr,w_rp from the 1 Run site cases, fit for the Bcd dependent parameters
@@ -366,7 +495,7 @@ for i=1:3
     % Range that is set as an initial guess. We will get a common set of
     % APbins that does not have NaN values in these AP bins.
     APbin_start = 20/2.5 + 1;
-    APbin_end = 45/2.5 + 1;
+    APbin_end = 50/2.5 + 1;
 
     APbinRange = (APbin_start:APbin_end)';
 
@@ -421,7 +550,7 @@ for i=1:3
 
     % Bounds of the parameters
     LB = [0.1, 1, 0, 50, 0.1 0.1 0 0 ];
-    UB = [10^2, 10^2, 1, 500, 10^2, 10^2, 1, 1];
+    UB = [10^2, 10^2, 0.5, 500, 10^2, 10^2, 1, 1];
 
 
     for j = 1:length(names)
@@ -511,7 +640,7 @@ ecornerplot(m,'names',names);
 % saveas(gcf,[FigPath,filesep,'Corner_plot_', constructNames{construct} ,'.pdf']); 
 
 %% Let's use the inferred [Kb, w_bp, p, R_max] to see if the models with fixed repressor parameters can predict the 2 Run sites
-
+clear Prediction
 for i=1:3
     params_Bcd_temp = mean(MCMC_2RunSites_global(i).chain(n_burn+1:end,:));
         if i==1 % r2-new : [011]
@@ -579,7 +708,7 @@ for i=1:3
     ylabel({'initial RNAP', 'loading rate (AU/min)'})
 
     StandardFigure(gcf,gca)
-    pause
+
 %   %Save the plot
 %     saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.tif']); 
 %     saveas(gcf,[FigPath,filesep,'Prediction_HillV3_',constructNames{construct},'.pdf']);
